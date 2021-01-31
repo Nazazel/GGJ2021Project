95b5305d61afbe7a17a797e2537cf46d9d2494c3
Pita Bread <pdoriadev@gmail.com>
2021-01-31 14:21:35 -0800

    Level 2 Blockout near-complete. Fixed hiding spot sprite scaling.

---
+++ GGJ2021Project/New Unity Project/Assets/Scripts/General/BackgroundPanner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundPanner : MonoBehaviour
+{
+    [SerializeField]
+    private float m_Frequency, m_Amplitude, m_MaxTimeSinceSwitch;
+    private float m_TimeSinceSwitch = 0;
+    private float m_XPosLastUpdate = 0;
+
+    private void LateUpdate()
+    {
+        transform.position += transform.right * m_Amplitude * Mathf.Sin(2 * Mathf.PI * m_Frequency * Time.time);
+
+
+    }
+}
