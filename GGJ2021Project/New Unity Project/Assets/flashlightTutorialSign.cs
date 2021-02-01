e0db0e0166e8ce93ca41c447cd06a151093e1dab
Pita Bread <pdoriadev@gmail.com>
2021-01-31 20:31:40 -0800

    Level1 Added signs. Intro area lighting.

---
+++ GGJ2021Project/New Unity Project/Assets/flashlightTutorialSign.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flashlightTutorialSign : MonoBehaviour
+{
+    public GameObject signOne, signTwo;
+    float t = 0;
+    private void LateUpdate()
+    {
+        t += Time.deltaTime;
+        if (t > 0.9f)
+        {
+            signOne.SetActive(!signOne.activeSelf);
+            signTwo.SetActive(!signTwo.activeSelf);
+            t = 0;
+        }
+    }
+}
