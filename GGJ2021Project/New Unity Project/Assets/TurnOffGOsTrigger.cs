e5bde55d3ed6925e02f808540680ae4f6b890876
Pita Bread <pdoriadev@gmail.com>
2021-02-03 21:47:22 -0800

    Updated hiding prefabs and lvl1. Completable

---
+++ GGJ2021Project/New Unity Project/Assets/TurnOffGOsTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOffGOsTrigger : MonoBehaviour
+{
+    public List<GameObject> TurnOffGOs = new List<GameObject>();
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            for (int i = 0; i < TurnOffGOs.Count; i++)
+            {
+                TurnOffGOs[i].SetActive(false);
+            }
+        }
+    }
+}
